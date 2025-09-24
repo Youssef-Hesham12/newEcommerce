0028b5c733fd1f42af4114bae98168df4241d196
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-09-24 15:19:14 +0300

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,22 @@
 import { apiServices } from "@/services/api";
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
-// import GoogleProvider from "next-auth/providers/google";
-// import GithubProvider from "next-auth/providers/github";
-// import FacebookProvider from "next-auth/providers/facebook";
+
 const handler = NextAuth({
   providers :[
     CredentialsProvider({
- // The name to display on the sign in form (e.g. "Sign in with...")
+
     name: "Credentials",
-    // `credentials` is used to generate a form on the sign in page.
-    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    // e.g. domain, username, password, 2FA token, etc.
-    // You can pass any HTML attribute to the <input> tag through the object.
+    
     credentials: {
       email: { label: "Email", type: "email", placeholder: "your-email@example.com" },
       password: { label: "Password", type: "password",placeholder:"********" }
     },
-    async authorize(credentials, req) {
+    async authorize(credentials) {
 
-      credentials?.email
-      credentials?.password
+        console.log("crdddddddddddddddddddddddddddddddddddddddd",credentials)
       const response = await apiServices.login(credentials?.email??"",credentials?.password??"")
-      console.log(response);
+      console.log("resssssssssssssssssssssssssponse",response);
       
 
       // Add logic here to look up the user from the credentials supplied
